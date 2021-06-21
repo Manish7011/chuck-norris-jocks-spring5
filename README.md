dcc2c5749380d9376294110123db699d184237b6
Manish7011 <manish.bhadani7011@gmail.com>
2021-06-21 09:45:15 +0530

    Initial Project Setup. Closes #1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # chuck-norris-jocks-spring5
+
 chuck-norris-jocks-spring5 is spring mvc web application. Which can deliver a new jock on refresh.
---
+++ src/main/java/bm/learning/chucknorrisjocksspring5/ChuckNorrisJocksSpring5Application.java
@@ -0,0 +1,13 @@
+package bm.learning.chucknorrisjocksspring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJocksSpring5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJocksSpring5Application.class, args);
+    }
+
+}
